732c07bf0eb53f56fa2b77dc7f6bd453901aa111
Qian Qian <Qian.Qian@bmw.de>
2022-03-05 10:27:48 +0100

    Basic running verison of dqn

---
+++ p1_navigation/model.py
@@ -0,0 +1,46 @@
+import copy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        base_size=32
+        self.fc_bb_1 = nn.Linear(state_size, base_size*16)
+        self.fc_bb_2 = nn.Linear(base_size*16, base_size*16)
+        self.fc_bb_3 = nn.Linear(base_size*16, base_size*8)
+
+        self.fc_s_head_1 = nn.Linear(base_size*8, base_size*4)
+        self.fc_s_head_2 = nn.Linear(base_size*4, 1)
+
+        self.fc_a_head_1 = nn.Linear(base_size*8, base_size*4)
+        self.fc_a_head_2 = nn.Linear(base_size*4, action_size)
+
+
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x=F.relu(self.fc_bb_1(state))
+        x=F.relu(self.fc_bb_2(x))
+        base=F.relu(self.fc_bb_3(x))
+
+        x1 = F.relu(self.fc_s_head_1(base))
+        state_value = self.fc_s_head_2(x1)
+
+        x2 = F.relu(self.fc_a_head_1(base))
+        action_values = self.fc_a_head_2(x2)
+        return state_value+action_values
---
+++ p1_navigation/navigation.py
@@ -0,0 +1,104 @@
+from unityagents import UnityEnvironment
+import numpy as np
+import torch
+from collections import deque
+from dqn_agent import Agent
+import matplotlib.pyplot as plt
+
+
+def get_next_state(env_info):
+    next_state = env_info.vector_observations[0]  # get the next state
+    return next_state
+
+def get_reward(env_info):
+    reward = env_info.rewards[0]  # get the reward
+    return reward
+
+
+def get_done(env_info):
+    done = env_info.local_done[0]  # see if episode has finished
+    return done
+
+def get_env_step_results(env_info):
+    return get_next_state(env_info), get_reward(env_info), get_done(env_info)
+
+class Environment(object):
+    def __init__(self, env, seed=42):
+        self._env = env
+        self._brain_name = env.brain_names[0]
+        self._brain = env.brains[self._brain_name]
+        self._env_info = env.reset(train_mode=True)[self._brain_name]
+
+        # number of agents in the environment
+        print('Number of agents:', len(self._env_info.agents))
+
+        # number of actions
+        self._action_size = self._brain.vector_action_space_size
+        print('Number of actions:', self._action_size)
+
+        # examine the state space
+        state = self._env_info.vector_observations[0]
+        print('States look like:', state)
+        self._state_size = len(state)
+        print('States have length:', self._state_size)
+        self._seed= seed
+        self._agent = Agent(self._state_size, self._action_size, self._seed)
+
+    def close(self):
+        self._env.close()
+
+    def train(self, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+        """Deep Q-Learning.
+
+            Params
+            ======
+                n_episodes (int): maximum number of training episodes
+                max_t (int): maximum number of timesteps per episode
+                eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+                eps_end (float): minimum value of epsilon
+                eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+            """
+        scores = []  # list containing scores from each episode
+        scores_window = deque(maxlen=100)  # last 100 scores
+        eps = eps_start
+
+        for i_episode in range(1, n_episodes + 1):
+            env_info = self._env.reset(train_mode=True)[self._brain_name]
+            state = get_next_state(env_info)
+            score = 0
+            for t in range(max_t):
+                action = self._agent.act(state, eps)
+                env_info = self._env.step(action)[self._brain_name]  # send the action to the environment
+                next_state, reward, done = get_env_step_results(env_info)
+                score += reward  # update the score
+                state = next_state  # roll over the state to next time step
+                if done:
+                    break
+            scores_window.append(score)  # save most recent score
+            scores.append(score)  # save most recent score
+            eps = max(eps_end, eps_decay * eps)  # decrease epsilon
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+            if i_episode % 100 == 0:
+                print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            if np.mean(scores_window) >= 10.0:
+                print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+                                                                                             np.mean(scores_window)))
+                torch.save(self._agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+                break
+        return scores
+
+def plot_scores(scores):
+    # plot the scores
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
+    
+if __name__ == "__main__":
+    env = Environment(UnityEnvironment(file_name="Banana_Linux/Banana.x86_64"))
+    scores = env.train()
+    plot_scores(scores)
+    env.close()
+
